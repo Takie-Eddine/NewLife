46471ec16921ef3d84f4fd66ff8839d79e1a918a
yacoubdemire <yacoubdemire@gmail.com>
2023-09-22 15:06:05 +0100

    binding

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public/../../public_html/newlifedashboard.life-nvst.com'),
+            'root' => storage_path('app/../../public_html/newlifedashboard.life-nvst.com'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
