13208c84acd88755fc2a297fc33178429371e5ce
yacoubdemire <yacoubdemire@gmail.com>
2023-09-22 15:53:47 +0100

    binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191) ;
 
-        $this->app->bind('path.public', function (){
-            return base_path().'/../public_html/newlifedashboard.life-nvst.com';
-        });
     }
 
     /**
