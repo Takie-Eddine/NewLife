2bbbfcfd07ce48896d5350461fd422cb330a34df
yacoubdemire <yacoubdemire@gmail.com>
2023-09-22 11:46:29 +0100

    chatting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         Schema::defaultStringLength(191) ;
+
+        $this->app->bind('path.public', function () {
+            return base_path().'/../public_html/newlifedashboard.life-nvst.com';
+        });
     }
 
     /**
